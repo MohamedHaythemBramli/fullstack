16dc90c0cefd87017051161fed0ab56363f7eb4d
Mohamed Haythem Bramli <mohamedhaythembramli90@gmail.com>
2024-10-12 17:23:14 +0200

    Fix Jwt Security

--- src/main/java/com/linkedin/backend/config/AuditorAwareConfig.java
+++ src/main/java/com/linkedin/backend/config/AuditorAwareConfig.java
@@ -1,8 +1,12 @@
 package com.linkedin.backend.config;
 
+import com.linkedin.backend.entities.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -10,16 +14,27 @@ import java.util.Optional;
 public class AuditorAwareConfig {
 
     @Bean
-    public AuditorAware<String> auditorAware() {
+    public AuditorAware<Integer> auditorAware() {
         return new AuditorAwareImpl();
     }
 }
 
-class AuditorAwareImpl implements AuditorAware<String> {
+class AuditorAwareImpl implements AuditorAware<Integer> {
 
     @Override
-    public Optional<String> getCurrentAuditor() {
-        // Return the currently authenticated user (e.g., from Spring Security)
-        return Optional.of("USER");  // Replace with actual user logic
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication =
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication();
+        if (authentication == null ||
+                !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken
+        ) {
+            return Optional.empty();
+        }
+
+        User userPrincipal = (User) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());
     }
 }
--- src/main/java/com/linkedin/backend/config/security/SecurityConfig.java
+++ src/main/java/com/linkedin/backend/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.linkedin.backend.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -38,6 +39,7 @@ public class SecurityConfig {
                                         "/swagger-ui/**",
                                         "/webjars/**",
                                         "/swagger-ui.html").permitAll()
+                                .requestMatchers("/api/customers/**").authenticated()
                                 .anyRequest().authenticated())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
